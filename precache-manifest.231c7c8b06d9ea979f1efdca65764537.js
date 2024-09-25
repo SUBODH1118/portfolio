d4542c2f7f43e1e47385addac4576216ee2a3816
SUBODH1118 <156596391+SUBODH1118@users.noreply.github.com>
2024-09-26 01:23:47 +0530

    Updates

---
+++ precache-manifest.231c7c8b06d9ea979f1efdca65764537.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8599f6177a6c7f91819fd71395672b61",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b26c8fd27f0e9b309c4b",
+    "url": "/portfolio/static/css/main.3ee74587.chunk.css"
+  },
+  {
+    "revision": "e735e61e99d1bfca3616",
+    "url": "/portfolio/static/js/2.4acb6f55.chunk.js"
+  },
+  {
+    "revision": "cbf92ed71fb4f2e94692ba67215d89b7",
+    "url": "/portfolio/static/js/2.4acb6f55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ac631b354279524b103",
+    "url": "/portfolio/static/js/3.a8aac0ea.chunk.js"
+  },
+  {
+    "revision": "b26c8fd27f0e9b309c4b",
+    "url": "/portfolio/static/js/main.8b58d928.chunk.js"
+  },
+  {
+    "revision": "48d1a264739a263ca0d5",
+    "url": "/portfolio/static/js/runtime-main.c64240e3.js"
+  },
+  {
+    "revision": "cf6e018f57bc41ae805065f815e13025",
+    "url": "/portfolio/static/media/InLink.cf6e018f.webp"
+  },
+  {
+    "revision": "cf6607eba0cbbc3912ac1d6c07828e08",
+    "url": "/portfolio/static/media/InsTA.cf6607eb.webp"
+  },
+  {
+    "revision": "e14379f917ef410c4ad008bb6b9e1c4f",
+    "url": "/portfolio/static/media/Myphoto.e14379f9.jpg"
+  },
+  {
+    "revision": "80f85e677e05efc346ef0505afb80b7b",
+    "url": "/portfolio/static/media/facebook.80f85e67.webp"
+  },
+  {
+    "revision": "e12d9989675c49e56407cc6fa62f760a",
+    "url": "/portfolio/static/media/food.e12d9989.png"
+  },
+  {
+    "revision": "66878ab2ccf32387ba3c3e6ec28d7914",
+    "url": "/portfolio/static/media/movies.66878ab2.jpg"
+  },
+  {
+    "revision": "cf6752ccc8c6faa6b7f00f4cd7cab38a",
+    "url": "/portfolio/static/media/things.cf6752cc.jpg"
+  },
+  {
+    "revision": "0bd2436092562bef42371a58f777264a",
+    "url": "/portfolio/static/media/treck.0bd24360.jpg"
+  },
+  {
+    "revision": "2b537929c96d5108056bd44f2d6dd4bf",
+    "url": "/portfolio/static/media/twit.2b537929.webp"
+  },
+  {
+    "revision": "a573fee5e0b1f8c944fd798c5894328f",
+    "url": "/portfolio/static/media/watchlist.a573fee5.jpg"
+  },
+  {
+    "revision": "f3f25700aed2698e833b80441bdbbf1e",
+    "url": "/portfolio/static/media/work1.f3f25700.jpg"
+  }
+]);
